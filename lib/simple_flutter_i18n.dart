4c30b2c1c55a6a77aaaabc19a320708c68b841a1
A7madXatab <Ahmadzendane725@gmail.com>
2020-02-08 09:01:07 -0800

    added dartdoc to the package

--- lib/simple_flutter_i18n.dart
+++ lib/simple_flutter_i18n.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 
-
 class I18n with ChangeNotifier {
+  /// the current langauge
   Map<String, dynamic> _currentLang;
   bool _isRtl = true;
 
+  /// creates an [I18n] instance ;
+  /// the [langauge] map should contain 'isRtl' key as the package is looking for that key
   I18n(Map<String, dynamic> language) {
     this._currentLang = language;
     this._isRtl = language['isRtl'];
   }
 
+  /// [lang] returns a copy of the current langauge
   Map<String, dynamic> get lang {
     return { ..._currentLang};
   }
 
+  /// returns the current boolean state of rtl in the current langauge
   bool get isRtl {
     return _isRtl;
   }
- 
+
+  /// updates the lanauge to [langauge] object.
+  /// the [langauge] Map must contain an 'isRtl' key as the package is looking for that key
   void changeLanguageTo(Map<String, dynamic> langauge) {
     _currentLang = langauge;
     _isRtl = langauge['isRtl'];
