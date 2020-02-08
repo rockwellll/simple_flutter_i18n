5377f1f7058e7c30c3f284538f697895534ffe99
A7madXatab <Ahmadzendane725@gmail.com>
2020-02-08 11:16:37 -0800

    now the class throws error if the locale dosent have a isRtl key

--- lib/simple_flutter_i18n.dart
+++ lib/simple_flutter_i18n.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-class I18n with ChangeNotifier {
+import './mixins/error_checker.dart';
+
+
+class I18n with ChangeNotifier, ErrorChecker  {
   /// the current langauge
   Map<String, dynamic> _currentLang;
   bool _isRtl = true;
@@ -8,13 +11,16 @@ class I18n with ChangeNotifier {
   /// creates an [I18n] instance ;
   /// the [langauge] map should contain 'isRtl' key as the package is looking for that key
   I18n(Map<String, dynamic> language) {
+    keyExistsOrError(language, 'isRtl');
+
+
     this._currentLang = language;
     this._isRtl = language['isRtl'];
   }
 
   /// [lang] returns a copy of the current langauge
   Map<String, dynamic> get lang {
-    return { ..._currentLang};
+    return Map.from(_currentLang);
   }
 
   /// returns the current boolean state of rtl in the current langauge
@@ -22,11 +28,14 @@ class I18n with ChangeNotifier {
     return _isRtl;
   }
 
-  /// updates the lanauge to [langauge] object.
-  /// the [langauge] Map must contain an 'isRtl' key as the package is looking for that key
-  void changeLanguageTo(Map<String, dynamic> langauge) {
-    _currentLang = langauge;
-    _isRtl = langauge['isRtl'];
+  /// updates the lanauge to [locale] object.
+  /// [locale] Map must contain an 'isRtl' key as the package is looking for that key
+  void setLocale(Map<String, dynamic> locale) {
+
+    keyExistsOrError(locale, 'isRtl');
+
+    _currentLang = locale;
+    _isRtl = locale['isRtl'];
 
     notifyListeners();
   }
