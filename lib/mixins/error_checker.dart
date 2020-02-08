1f0945bab906a07087bcee91b9061cfee8abe1d0
A7madXatab <Ahmadzendane725@gmail.com>
2020-02-08 11:15:24 -0800

    latest code

---
+++ lib/mixins/error_checker.dart
@@ -0,0 +1,11 @@
+import '../util/no_rtl_expection.dart';
+
+mixin ErrorChecker {
+  bool keyExistsOrError(Map src,String key) {
+    if(!src.containsKey(key)) {
+      throw new NoRtlExeption('the locale object dosent have a key named \'isRtl\'. please make sure that the locale map contains a isRtl key ');
+    }
+
+    return true;
+  }
+}
---
+++ lib/util/no_rtl_expection.dart
@@ -0,0 +1,11 @@
+
+class NoRtlExeption implements Exception {
+  final String message;
+
+  NoRtlExeption(this.message);
+
+   @override
+  String toString() {
+    return this.message;
+  }
+}
