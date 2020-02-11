5d3b35ce3c82de760b016a3ee282cdba8dcb8ff4
A7madXatab <Ahmadzendane725@gmail.com>
2020-02-11 01:01:29 -0800

    adding load and persist method to the package

--- lib/simple_flutter_i18n.dart
+++ lib/simple_flutter_i18n.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import './mixins/error_checker.dart';
+import './classes/storage_loader.dart';
+import './classes/storage_persister.dart';
 
-
-class I18n with ChangeNotifier, ErrorChecker  {
+class I18n with ChangeNotifier, ErrorChecker {
   /// the current langauge
   Map<String, dynamic> _currentLang;
   bool _isRtl = true;
@@ -12,7 +15,6 @@ class I18n with ChangeNotifier, ErrorChecker  {
   /// the [langauge] map should contain 'isRtl' key as the package is looking for that key
   I18n(Map<String, dynamic> language) {
     keyExistsOrError(language, 'isRtl');
-
 
     this._currentLang = language;
     this._isRtl = language['isRtl'];
@@ -31,12 +33,29 @@ class I18n with ChangeNotifier, ErrorChecker  {
   /// updates the lanauge to [locale] object.
   /// [locale] Map must contain an 'isRtl' key as the package is looking for that key
   void setLocale(Map<String, dynamic> locale) {
-
     keyExistsOrError(locale, 'isRtl');
 
     _currentLang = locale;
     _isRtl = locale['isRtl'];
 
     notifyListeners();
+  }
+
+  /// loads the language from the storage , and intializes it
+  void load() async {
+    final loader = new StorageLoader();
+
+    final encodedLanguage = await loader.load('language');
+    final language = json.decode(encodedLanguage);
+
+    this.setLocale(language);
+  }
+
+  ///persists the current language to the storage
+  Future<bool> persist() {
+    final persister = new StoragePersister();
+
+    return persister
+        .persist({'key': 'language', 'value': json.encode(this._currentLang)});
   }
 }
